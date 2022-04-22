7e5a1845b0faf7110f56b2bc9d744f76622708bc
RJudge1170 <ryantj@live.unc.edu>
2022-04-22 16:02:47 -0400

    image should display correctly

--- server.js
+++ server.js
@@ -9,21 +9,49 @@ args["port"]
 
 const port = args.port || process.env.PORT || 3000
 
-fs.readFile('./index.html', 'utf8', (err, data) => {
-    if (err) {
-        console.error(err)
-        return
+function sendFile(fname, contentType, req, res) {
+    res.writeHead(200, { 'Content-Type': contentType});
+    fs.readFile(fname, function(error, data) {
+        if(error)   {  
+            res.writeHead(404);
+            res.write('Error: File not Found');
+        } else {
+            res.write(data);
+        }
+        res.end();
+    })
+}
+
+const server = http.createServer(function(req, res){
+    if (req.url === "/") {
+        sendFile('index.html', 'text/html', req, res);
+    } else if (req.url === '/NorthCarolina.png') {
+        sendFile('NorthCarolina.png', 'image/gif', req, res);
+    } else {
+        res.writeHead(404);
+        res.end('Error: Unsupported path');
     }
-
-
-const server = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/html')
-    res.end(data)
-})
-
+});
 
 server.listen(port, () =>{
     console.log(`Server running at port ${port}`)
 })
-})
+
+// fs.readFile('./index.html', 'utf8', (err, data) => {
+//     if (err) {
+//         console.error(err)
+//         return
+//     }
+
+
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200
+//     res.setHeader('Content-Type', 'text/html')
+//     res.end(data)
+// })
+
+
+// server.listen(port, () =>{
+//     console.log(`Server running at port ${port}`)
+// })
+// })
