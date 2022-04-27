ce0a34e176cae45c3ea406fb3e0e478baab31af6
RJudge1170 <ryantj@live.unc.edu>
2022-04-26 21:42:32 -0400

    morgan now logs user page access information

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const http = require('http')
 
 const fs = require('fs')
 
+const express = require('express')
+const req = require('express/lib/request')
+const app = express()
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const morgan = require('morgan')
+
 const args = require('minimist')(process.argv.slice(2))
 
 args["port"]
@@ -21,33 +30,37 @@ function sendFile(fname, contentType, req, res) {
         res.end();
     })
 }
-
-// Logging Middleware
+// Create a write stream to append (flags: 'a') to a file
+const WRITESTREAM = fs.createWriteStream('accesslog', { flags: 'a' })
+var logger = morgan('combined', { stream: WRITESTREAM })
 
 const server = http.createServer(function(req, res){
-    if (req.url === "/") {
-        sendFile('index.html', 'text/html', req, res);
-    } else if (req.url === '/NorthCarolina.png') {
-        sendFile('NorthCarolina.png', 'image/gif', req, res);
-    } else if (req.url === '/northcarolinawelcome.jpeg') {
-        sendFile('northcarolinawelcome.jpeg', 'image/gif', req, res);
-    } else if (req.url === '/style.css') {
-        sendFile('style.css', 'text/html', req, res);
-    } else if (req.url === '/index.js') {
-        sendFile('index.js', 'text/javascript', req, res);
-    } else if (req.url === '/mainPage.html') {
-        sendFile('mainPage.html', 'text/html', req, res);
-    } else if (req.url === '/index.html') {
-        sendFile('index.html', 'text/html', req, res);
-    } else {
-        res.writeHead(404);
-        res.end('Error: Unsupported path');
-    }
+    logger(req, res, function (err) {
+        if (req.url === "/") {
+            sendFile('index.html', 'text/html', req, res);
+        } else if (req.url === '/NorthCarolina.png') {
+            sendFile('NorthCarolina.png', 'image/gif', req, res);
+        } else if (req.url === '/northcarolinawelcome.jpeg') {
+            sendFile('northcarolinawelcome.jpeg', 'image/gif', req, res);
+        } else if (req.url === '/style.css') {
+            sendFile('style.css', 'text/html', req, res);
+        } else if (req.url === '/index.js') {
+            sendFile('index.js', 'text/javascript', req, res);
+        } else if (req.url === '/mainPage.html') {
+            sendFile('mainPage.html', 'text/html', req, res);
+        } else if (req.url === '/index.html') {
+            sendFile('index.html', 'text/html', req, res);
+        } else {
+            res.writeHead(404);
+            res.end('Error: Unsupported path');
+        }
+    })
 });
 
 server.listen(port, () =>{
     console.log(`Server running at port ${port}`)
 })
+
 
 // fs.readFile('./mainPage.html', 'utf8', (err, data) => {
 //     if (err) {
